d344fbfaf515bb611e8f272eca845c26c6f2d273
satoo air <satoo>
2016-02-05 18:28:40 +0900

    update 1.1.3

--- Scripts/WebCamTextureMarkerBasedARSample.cs
+++ Scripts/WebCamTextureMarkerBasedARSample.cs
@@ -200,7 +200,7 @@ namespace MarkerBasedARSample
 				void Update ()
 				{
 
-						if (webCamTextureToMatHelper.isPlaying ()) {
+						if (webCamTextureToMatHelper.isPlaying () && webCamTextureToMatHelper.didUpdateThisFrame ()) {
 				
 								Mat rgbaMat = webCamTextureToMatHelper.GetMat ();
 
